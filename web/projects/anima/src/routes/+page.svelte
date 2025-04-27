d1cf98029b585c121e3f3ba31317f5ae3f4c0b84
Franciss <francisgil.prog@gmail.com>
2025-04-27 14:42:38 +0800

    yeet

---
+++ web/projects/anima/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<main class=" p-16 text-white">
+	<section
+		class="font-oswald relative h-screen w-full rounded-[44px] bg-cover bg-center"
+		style="background-image: url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp5139695.jpg&f=1&nofb=1&ipt=2abefdab61487fb67fcba9e5d5908dd884d4db12ff1acb1a8382bd536633ee50')"
+	>
+		<!-- Black Overlay -->
+		<div class="absolute inset-0 z-0 rounded-[44px] bg-black opacity-50"></div>
+
+		<div class="font-oswald flex h-full flex-col justify-between">
+			<header class="z-0 flex h-36 items-center justify-center px-8">
+				<nav class="z-0 flex h-20 w-full items-center justify-between bg-[#F9F6EE] px-8 text-black">
+					<span>asdasf</span>
+					<span>asdasf</span>
+				</nav>
+			</header>
+			<div class="flex h-full items-end justify-between">
+				<div class=" z-0 flex flex-col items-start gap-5 px-8 py-5 text-4xl">
+					<span class="font-michroma text-[100px]">ANIMA</span>
+					<span>All the anime info you need — in one place.</span>
+					<a
+						href="/anime"
+						class="font-michroma rounded-xl bg-white py-3 pr-7 pl-2 text-black hover:underline"
+						>Explore Now!</a
+					>
+				</div>
+				<!-- card -->
+				<div
+					class="z-0 flex h-[200px] w-lg flex-col justify-center gap-3 rounded-tl-[44px] bg-white px-8 py-5 text-5xl text-black"
+				>
+					<span> Explore titles, characters, episodes, and more.</span>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
