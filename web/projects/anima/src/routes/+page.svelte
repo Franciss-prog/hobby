c5d261b85d9ac1455cc0958f4b9a9d66ffd574e2
Franciss <francisgil.prog@gmail.com>
2025-04-30 07:59:39 +0800

    for backup purposes

--- web/projects/anima/src/routes/+page.svelte
+++ web/projects/anima/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<main class=" p-16 text-white">
+<main class=" bg-white p-16 text-white">
 	<section
 		class="font-oswald relative h-screen w-full rounded-[44px] bg-cover bg-center"
 		style="background-image: url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp5139695.jpg&f=1&nofb=1&ipt=2abefdab61487fb67fcba9e5d5908dd884d4db12ff1acb1a8382bd536633ee50')"
@@ -8,26 +8,29 @@
 
 		<div class="font-oswald flex h-full flex-col justify-between">
 			<header class="z-0 flex h-36 items-center justify-center px-8">
-				<nav class="z-0 flex h-20 w-full items-center justify-between bg-[#F9F6EE] px-8 text-black">
-					<span>asdasf</span>
-					<span>asdasf</span>
+				<nav
+					class="font-poppins z-0 flex h-20 w-full items-center justify-between rounded-xl bg-white px-8 text-xl text-black"
+				>
+					<a href="/" class="text-2xl font-bold"> 語 </a>
+					<!-- navlinks -->
+					<div class="flex items-center gap-5">
+						<a href="/" class="hover:underline">Anime</a>
+						<a href="/" class="hover:underline">Manga</a>
+						<a href="/" class="hover:underline">Characters</a>
+					</div>
+					<a href="/anime" class="hover:underline">Explore Now</a>
 				</nav>
 			</header>
 			<div class="flex h-full items-end justify-between">
 				<div class=" z-0 flex flex-col items-start gap-5 px-8 py-5 text-4xl">
 					<span class="font-michroma text-[100px]">ANIMA</span>
 					<span>All the anime info you need — in one place.</span>
-					<a
-						href="/anime"
-						class="font-michroma rounded-xl bg-white py-3 pr-7 pl-2 text-black hover:underline"
-						>Explore Now!</a
-					>
 				</div>
 				<!-- card -->
 				<div
 					class="z-0 flex h-[200px] w-lg flex-col justify-center gap-3 rounded-tl-[44px] bg-white px-8 py-5 text-5xl text-black"
 				>
-					<span> Explore titles, characters, episodes, and more.</span>
+					<span> Explore anime, manga, characters , and more.</span>
 				</div>
 			</div>
 		</div>
